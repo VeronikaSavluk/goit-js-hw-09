400a9199c71859910c9d974bb3ae5da01be9f4cd
Veronika Savluk <s_veronica@ukr.net>
2022-08-03 19:21:57 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const body = document.querySelector("body");
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
+let timerChangeColorBody = null;
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick(event) {
+    startBtn.disabled = true;
+    timerChangeColorBody = setInterval(function() { body.style.backgroundColor = getRandomHexColor() }, 1000);
+};
+
+function onStopBtnClick(event) {
+    startBtn.disabled = false;
+    clearInterval(timerChangeColorBody);
+};
+console.log(body);
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
