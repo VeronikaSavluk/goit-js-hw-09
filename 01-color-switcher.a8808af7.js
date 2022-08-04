0f874d527731cd42139f0a6f78297f23f13cc7ae
VeronikaSavluk <98710326+VeronikaSavluk@users.noreply.github.com>
2022-08-04 22:30:06 +0000

    Deploying to gh-pages from @ VeronikaSavluk/goit-js-hw-09@3238d69015e4364ecfe4bdfeaf4ea7fc32f29f74 🚀

--- 01-color-switcher.a8808af7.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),o=null;e.addEventListener("click",(function(n){e.disabled=!0,o=setInterval((function(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),n.addEventListener("click",(function(t){e.disabled=!1,clearInterval(o)})),console.log(t)}();
-//# sourceMappingURL=01-color-switcher.a8808af7.js.map
